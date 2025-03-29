d4859d3b37bc50ef79e357f98bda19181e2efd02
Jatt <jatt.jatt@gmail.com>
2025-03-29 10:18:39 -0500

    update to strapi 5.12.1

--- src/admin/vite.config.js
+++ src/admin/vite.config.js
@@ -3,7 +3,7 @@ const { mergeConfig } = require('vite');
 module.exports = (config) => {
   return mergeConfig(config, {
     server: {
-      allowedHosts: ['manager.serviciolegal.com.co','manager.lsconsulting.com.co' ],
+      allowedHosts: ['manager.serviciolegal.com.co','manager.lsconsulting.com.co', 'dev.einscube.com' ],
     },
     resolve: {
       alias: {
