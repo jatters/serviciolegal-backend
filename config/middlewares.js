b6b5f52530b570b59888bf82dd2779827526ada9
Jatt <jatt.jatt@gmail.com>
2025-04-29 09:27:30 -0500

    update strapi to version 5.12.6 and fix cors issue

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,32 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://serviciolegal.com.co",
+            "https://manager.serviciolegal.com.co",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "https://serviciolegal.com.co",
+            "https://manager.serviciolegal.com.co",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
