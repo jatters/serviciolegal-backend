1d1d416487f452e1fd0ec9c4325ef17d534cdc25
Jatt <jatt.jatt@gmail.com>
2026-01-26 12:49:24 -0500

    update setting to post Webhook to refresh content

--- config/server.js
+++ config/server.js
@@ -7,5 +7,8 @@ module.exports = ({ env }) => ({
   },
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+    defaultHeaders: {
+      Authorization: `Bearer ${env("WEBHOOK_REVALIDATE_SECRET", "")}`,
+    },
   },
 });
